7673f917729f2bfdcb9681d272272fe384b7aafb
Carlos <jjbbpe@gmail.com>
2020-10-19 01:47:11 +0200

    agenda de contactos

--- index.php
+++ index.php
@@ -9,8 +9,9 @@
             <a class="button bg-1 c-w" href="http://getskeleton.com/" target="_blank">Skeleton</a>
             <a class="button bg-2 c-w" href="sites/mq.php" target="_blank">MQuery</a>
             <a class="button bg-3 c-w" href="sites/index-of.php" target="_blank">Index Of</a>
-            <a class="button bg-4 c-w" href="ubungen/formulario/formulario.php" target="_blank">Formulario</a>
+            <a class="button bg-4 c-w" href="ubungen/formulario/index.php" target="_blank">Formulario</a>
             <a class="button bg-5 c-w" href="sites/tabla-multiplicar.php" target="_blank">Tabla de multiplicar</a>
+            <a class="button bg-1 c-w" href="ubungen/agendaphp/index.php" target="_blank">Agenda PHP</a>
          </div>
 
       </div> <!-- row -->
--- sites/tabla-multiplicar.php
+++ sites/tabla-multiplicar.php
@@ -3,9 +3,40 @@
 
 <div class="container">
    <div id="contenidoTablas" class="row">
+      <h3 class="my-2 text-center c-1">Tabla de Multiplicar</h3>
 
    </div> <!-- Contenido tablas-->
+   <div class="row">
+<pre>
+<code>
+function tabla(numero){
+  const tablaID = '#t'+ numero;
+  const multi = 'multi' + numero;
+  const multiID = '#'+multi;
 
+   document.querySelector('#contenidoTablas').innerHTML += `
+
+     &ltdiv id="${tablaID}" class="u-pull-left" style="margin:0 5rem"&gt
+          &lth4&gtX  ${numero}&lt/h4&gt
+          &ltul id='${multi}' class="multiplicacion"&gt
+          &lt/ul&gt
+     &lt/div&gt
+
+    `
+   for(let i=1; i<=12; i++){
+       document.querySelector(multiID).innerHTML += `
+       &ltli style="list-style:none"&gt${numero} x ${i} = &ltspan style="font-weight:bold "&gt${i*numero}&lt/span&gt &lt/li&gt
+       `
+     }
+}
+
+for(let i=1; i<=12; i++){
+   tabla(i)
+}
+</code>
+</pre>
+
+   </div>
 </div>
 
 
@@ -17,15 +48,17 @@
 
       document.querySelector('#contenidoTablas').innerHTML += `
 
-        <div id="${tablaID}" class="u-pull-left">
-             <h2>X  ${numero}</h2>
+        <div id="${tablaID}" class="u-pull-left" style="margin:0 5rem">
+             <h4>X  ${numero}</h4>
              <ul id='${multi}' class="multiplicacion">
              </ul>
         </div>
 
        `
       for(let i=1; i<=12; i++){
-          document.querySelector(multiID).innerHTML += ` <li >${numero} x ${i} = <span>${i*numero}</span> </li> `
+          document.querySelector(multiID).innerHTML += `
+          <li style="list-style:none">${numero} x ${i} = <span  class="c-1" style="font-weight:bold ">${i*numero}</span> </li>
+          `
         }
    }
 
