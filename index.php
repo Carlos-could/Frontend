e2803c9153b5348a75fc70c577cecd63ee85233a
Carlos-could <jjbbpe@gmail.com>
2020-10-29 23:38:56 +0100

    -agenda

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
             <a class="button bg-5 c-w" href="sites/tabla-multiplicar.php" target="_blank">Tabla de multiplicar</a>
             <a class="button bg-1 c-w" href="ubungen/agendaphp/index.php" target="_blank">Agenda PHP</a>
             <a class="button bg-2 c-w" href="sites/ajax.php" target="_blank">Ajax</a>
-            <a class="button bg-5 c-w" href="sites/antesDeReact.php" target="_blank">JS Resumen</a>
+            <a class="button bg-3 c-w" href="sites/antesDeReact.php" target="_blank">JS Resumen</a>
          </div>
 
       </div> <!-- row -->
