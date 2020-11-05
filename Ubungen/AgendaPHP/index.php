91d427ea1a943acdf6d48f32bd9dd59be3b217be
Carlos-could <jjbbpe@gmail.com>
2020-11-05 15:13:57 +0100

    crud

--- Ubungen/AgendaPHP/index.php
+++ Ubungen/AgendaPHP/index.php
@@ -2,7 +2,7 @@
 
 <div class="container">
    <div class="row my-4 text-center">
-           <h3 class="">Agenda de Contactos</h3>
+           <h3 class="">Agenda de Contactos - CRUD</h3>
    </div>
 
     <div class="row">
