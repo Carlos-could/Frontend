1f06eeda19fafb3fd98701f821c2dd3910aa21bb
Carlos-could <jjbbpe@gmail.com>
2020-11-05 23:32:46 +0100

    por fin video 374, pero con dudas

--- Ubungen/AgendaPHP/inc/modelos/modelo-contactos.php
+++ Ubungen/AgendaPHP/inc/modelos/modelo-contactos.php
@@ -1,38 +1,35 @@
 <?php
-echo json_encode($_POST);
-// if ($_POST["accion"] == "crear") {
-//   // creará un nuevo registro en la base de datos
-//
-//   require_once('../funciones/bd.php');
-//   // echo json_encode($_POST);
-//   //Validar las entradas
-//
-//   $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
-//   $empresa = filter_var($_POST['empresa'], FILTER_SANITIZE_STRING);
-//   $telefono = filter_var($_POST['telefono'], FILTER_SANITIZE_STRING);
-//   //
-//   try {
-//     $stmt = $conn->prepare("INSERT INTO contactos (nombre, empresa, telefono) VALUES (?, ?, ?)");
-//     $stmt->bind_param("sss", $nombre, $empresa, $telefono);
-//     $stmt->execute();
-//     if($stmt->affected_rows == 1) {
-//       $respuesta = array(
-//         'respuesta' => 'correcto',
-//         'datos' => array(
-//           'nombre' => $nombre,
-//           'empresa' => $empresa,
-//           'telefono' => $telefono,
-//           'id_insertado' => $stmt->insert_id
-//         )
-//       );
-//     }
-//
-//
-//     $stmt->close();
-//     $conn->close();
-//     } catch(Exception $e) {
-//       $respuesta = array(
-//         'error' = $e->getMessage()
-//       );
-//     } //catch
-// } //if
+if ($_POST['accion'] == 'crear'){
+  // creará un nuevo registro en la base de datos
+
+  require_once('../funciones/bd.php');
+
+  //Validar las entradas
+  $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
+  $empresa = filter_var($_POST['empresa'], FILTER_SANITIZE_STRING);
+  $telefono = filter_var($_POST['telefono'], FILTER_SANITIZE_STRING);
+
+  try {
+    $stmt = $conn->prepare("INSERT INTO contactos (nombre, empresa, telefono) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $nombre, $empresa, $telefono);
+    $stmt->execute();
+    if ($stmt->affected_rows == 1) {
+      $respuesta = array(
+        'respuesta' => 'correcto',
+        'datos' => array(
+          'nombre' => $nombre,
+          'empresa' => $empresa,
+          'telefono' => $telefono,
+          'id_insertado' => $stmt->insert_id
+        )
+      );
+    }
+    $stmt->close();
+    $conn->close();
+    } catch(Exception $e) {
+      $respuesta = array(
+        'error' => $e->getMessage()
+      );
+    }
+  echo json_encode($respuesta);
+}
