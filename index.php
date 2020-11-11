95a3c720e824def4090b73646d7b4f74c2522fa6
Carlos-could <jjbbpe@gmail.com>
2020-11-11 20:01:28 +0100

    orden

--- index.php
+++ index.php
@@ -3,22 +3,23 @@
 <body>
    <div class="container">
       <div class="row">
-
-         <div class="u-full-width">
-            <h1 class="text-center my-4">Front-end</h1>
-            <a class="button bg-1 c-w" href="http://getskeleton.com/" target="_blank">Skeleton</a>
-            <a class="button bg-2 c-w" href="sites/mq.php" target="_blank">MQuery</a>
-            <a class="button bg-3 c-w" href="sites/index-of.php" target="_blank">Index Of</a>
-            <a class="button bg-4 c-w" href="ubungen/formulario/index.php" target="_blank">Formulario</a>
-            <a class="button bg-5 c-w" href="sites/tabla-multiplicar.php" target="_blank">Tabla de multiplicar</a>
-            <a class="button bg-1 c-w" href="ubungen/agendaphp/index.php" target="_blank">Agenda PHP</a>
-            <a class="button bg-2 c-w" href="sites/ajax.php" target="_blank">Ajax</a>
-            <a class="button bg-3 c-w" href="sites/antesDeReact.php" target="_blank">JS Resumen</a>
-         </div>
-
+        <h1 class="text-center my-4">Front-end</h1>
+        <a class="button bg-1 c-w" href="http://getskeleton.com/" target="_blank">Skeleton</a>
+        <a class="button bg-1 c-w" href="sites/mq.php" target="_blank">MQuery</a>
       </div> <!-- row -->
+      <div class="row">
+        <a class="button bg-2 c-w" href="sites/index-of.php" target="_blank">Index Of</a>
+        <a class="button bg-2 c-w" href="sites/tabla-multiplicar.php" target="_blank">Tabla de multiplicar</a>
+      </div>
+      <div class="row">
+        <a class="button bg-3 c-w" href="sites/antesDeReact.php" target="_blank">JS Resumen</a>
+        <a class="button bg-3 c-w" href="sites/formulario/index.php" target="_blank">Formulario</a>
+        <a class="button bg-3 c-w" href="sites/ajax.php" target="_blank">Ajax</a>
+      </div>
+      <div class="row">
+        <a class="button bg-4  c-w" href="sites/agendaphp/index.php" target="_blank">Agenda PHP</a>
+      </div>
    </div> <!-- container -->
-
    <script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
